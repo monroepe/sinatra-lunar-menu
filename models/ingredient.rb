09bbef1f41c4dca81848067011ca06ebee8f5d80
Peter <monroepe@gmail.com>
2014-09-05 13:02:23 -0400

    All tests passed

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,11 @@
 class Ingredient
+
+  attr_reader :id, :name
+
+  def initialize(id, name, recipe_id)
+    @id = id
+    @name = name
+    @recipe_id = recipe_id
+  end
+
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require 'pg'
+require_relative 'ingredient'
 
 class Recipe
 
@@ -11,6 +12,14 @@ class Recipe
     @instructions = instructions
     @description = description
   end
+
+  def ingredients
+    self.class.find_ingredients(id)
+  end
+
+####################
+#Class Methods
+####################
 
   def self.db_connection
     begin
@@ -41,6 +50,46 @@ class Recipe
     recipes
   end
 
+  def self.find(id)
+    query = 'SELECT *
+    FROM recipes
+    WHERE recipes.id = $1;'
+
+    recipe = db_connection do |conn|
+      conn.exec_params(query, [id])
+    end
+
+    instructions = "This recipe doesn't have any instructions."
+    description  = "This recipe doesn't have a description."
+
+    if recipe[0]['instructions'] == nil
+      instructions = "This recipe doesn't have any instructions."
+    else
+      instructions = recipe[0]['instructions']
+    end
+    if recipe[0]['description'] == nil
+      description  = "This recipe doesn't have a description."
+    else
+      description = recipe[0]['description']
+    end
+
+    Recipe.new(recipe[0]['id'], recipe[0]['name'], instructions, description)
+  end
+
+  def self.find_ingredients(id)
+    ingredients = []
+
+    query = 'SELECT * FROM ingredients
+    WHERE ingredients.recipe_id = $1;'
+
+    data = db_connection do |conn|
+      conn.exec_params(query, [id])
+    end
+    data.each do |ingredient|
+      ingredients << Ingredient.new(ingredient['id'], ingredient['name'], ingredient['recipe_id'])
+    end
+    ingredients
+  end
 
 end
 
